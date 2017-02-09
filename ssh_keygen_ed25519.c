59126d3c974714a3498eb657b49078eb22cc3e30
Peter Szabo <pts@fazekas.hu>
2017-02-09 13:12:40 +0100

    updated ssh_keygen_ed25519.c

--- ssh_keygen_ed25519.c
+++ ssh_keygen_ed25519.c
@@ -1,69 +1,481 @@
 /* by pts@fazekas.hu at Thu Jan 26 15:04:16 CET 2017
+ * ... Thu Feb  9 12:22:55 CET 2017
  *
- * gcc -W -Wall -Wextra -Werror -ffunction-sections -fdata-sections -Wl,--gc-sections -s -Os -o tgen tgen.c
+ * gcc -W -Wall -Wextra -Werror -ffunction-sections -fdata-sections -Os -o tgen tgen.c
  */
 
 #if __XTINY__
 #include <xtiny.h>
 #else
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #endif
 
-#include "tweetnacl.c"
+/* --- ed25519 crypto based on tweetnacl.c 20140427 */
 
-/* !! xtiny */
+#define FOR(i,n) for (i = 0;i < n;++i)
+
+typedef uint8_t u8;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int64_t i64;
+typedef i64 gf[16];
+
+static const u8
+  _0[16],
+  _9[32] = {9};
+static const gf
+  gf0,
+  gf1 = {1},
+  _121665 = {0xDB41,1},
+  D = {0x78a3, 0x1359, 0x4dca, 0x75eb, 0xd8ab, 0x4141, 0x0a4d, 0x0070, 0xe898, 0x7779, 0x4079, 0x8cc7, 0xfe73, 0x2b6f, 0x6cee, 0x5203},
+  D2 = {0xf159, 0x26b2, 0x9b94, 0xebd6, 0xb156, 0x8283, 0x149a, 0x00e0, 0xd130, 0xeef3, 0x80f2, 0x198e, 0xfce7, 0x56df, 0xd9dc, 0x2406},
+  X = {0xd51a, 0x8f25, 0x2d60, 0xc956, 0xa7b2, 0x9525, 0xc760, 0x692c, 0xdc5c, 0xfdd6, 0xe231, 0xc0a4, 0x53fe, 0xcd6e, 0x36d3, 0x2169},
+  Y = {0x6658, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666, 0x6666},
+  I = {0xa0b0, 0x4a0e, 0x1b27, 0xc4ee, 0xe478, 0xad2f, 0x1806, 0x2f43, 0xd7a7, 0x3dfb, 0x0099, 0x2b4d, 0xdf0b, 0x4fc1, 0x2480, 0x2b83};
+
+static u64 dl64(const u8 *x) {
+  u64 i,u=0;
+  FOR(i,8) u=(u<<8)|x[i];
+  return u;
+}
+
+static void ts64(u8 *x,u64 u) {
+  int i;
+  for (i = 7;i >= 0;--i) { x[i] = u; u >>= 8; }
+}
+
+static const u8 sigma[16] = "expand 32-byte k";
+
+static const u32 minusp[17] = {
+  5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 252
+} ;
+
+static void set25519(gf r, const gf a) {
+  int i;
+  FOR(i,16) r[i]=a[i];
+}
+
+static void car25519(gf o) {
+  int i;
+  i64 c;
+  FOR(i,16) {
+    o[i]+=(1LL<<16);
+    c=o[i]>>16;
+    o[(i+1)*(i<15)]+=c-1+37*(c-1)*(i==15);
+    o[i]-=c<<16;
+  }
+}
+
+static void sel25519(gf p,gf q,int b) {
+  i64 t,i,c=~(b-1);
+  FOR(i,16) {
+    t= c&(p[i]^q[i]);
+    p[i]^=t;
+    q[i]^=t;
+  }
+}
+
+static void pack25519(u8 *o,const gf n) {
+  int i,j,b;
+  gf m,t;
+  FOR(i,16) t[i]=n[i];
+  car25519(t);
+  car25519(t);
+  car25519(t);
+  FOR(j,2) {
+    m[0]=t[0]-0xffed;
+    for(i=1;i<15;i++) {
+      m[i]=t[i]-0xffff-((m[i-1]>>16)&1);
+      m[i-1]&=0xffff;
+    }
+    m[15]=t[15]-0x7fff-((m[14]>>16)&1);
+    b=(m[15]>>16)&1;
+    m[14]&=0xffff;
+    sel25519(t,m,1-b);
+  }
+  FOR(i,16) {
+    o[2*i]=t[i]&0xff;
+    o[2*i+1]=t[i]>>8;
+  }
+}
+
+static u8 par25519(const gf a) {
+  u8 d[32];
+  pack25519(d,a);
+  return d[0]&1;
+}
+
+static void A(gf o,const gf a,const gf b) {
+  int i;
+  FOR(i,16) o[i]=a[i]+b[i];
+}
+
+static void Z(gf o,const gf a,const gf b) {
+  int i;
+  FOR(i,16) o[i]=a[i]-b[i];
+}
+
+static void M(gf o,const gf a,const gf b) {
+  i64 i,j,t[31];
+  FOR(i,31) t[i]=0;
+  FOR(i,16) FOR(j,16) t[i+j]+=a[i]*b[j];
+  FOR(i,15) t[i]+=38*t[i+16];
+  FOR(i,16) o[i]=t[i];
+  car25519(o);
+  car25519(o);
+}
+
+static void S(gf o,const gf a) {
+  M(o,a,a);
+}
+
+static void inv25519(gf o,const gf i) {
+  gf c;
+  int a;
+  FOR(a,16) c[a]=i[a];
+  for(a=253;a>=0;a--) {
+    S(c,c);
+    if(a!=2&&a!=4) M(c,c,i);
+  }
+  FOR(a,16) o[a]=c[a];
+}
+
+static u64 R(u64 x,int c) { return (x >> c) | (x << (64 - c)); }
+static u64 Ch(u64 x,u64 y,u64 z) { return (x & y) ^ (~x & z); }
+static u64 Maj(u64 x,u64 y,u64 z) { return (x & y) ^ (x & z) ^ (y & z); }
+static u64 Sigma0(u64 x) { return R(x,28) ^ R(x,34) ^ R(x,39); }
+static u64 Sigma1(u64 x) { return R(x,14) ^ R(x,18) ^ R(x,41); }
+static u64 sigma0(u64 x) { return R(x, 1) ^ R(x, 8) ^ (x >> 7); }
+static u64 sigma1(u64 x) { return R(x,19) ^ R(x,61) ^ (x >> 6); }
+
+static const u64 K[80] =  {
+  0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL, 0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL,
+  0x3956c25bf348b538ULL, 0x59f111f1b605d019ULL, 0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL,
+  0xd807aa98a3030242ULL, 0x12835b0145706fbeULL, 0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL,
+  0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL, 0x9bdc06a725c71235ULL, 0xc19bf174cf692694ULL,
+  0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL, 0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
+  0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL, 0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL,
+  0x983e5152ee66dfabULL, 0xa831c66d2db43210ULL, 0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL,
+  0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL, 0x06ca6351e003826fULL, 0x142929670a0e6e70ULL,
+  0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL, 0x4d2c6dfc5ac42aedULL, 0x53380d139d95b3dfULL,
+  0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL, 0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
+  0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL, 0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL,
+  0xd192e819d6ef5218ULL, 0xd69906245565a910ULL, 0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL,
+  0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL, 0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL,
+  0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL, 0x5b9cca4f7763e373ULL, 0x682e6ff3d6b2b8a3ULL,
+  0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL, 0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
+  0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL, 0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL,
+  0xca273eceea26619cULL, 0xd186b8c721c0c207ULL, 0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL,
+  0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL, 0x113f9804bef90daeULL, 0x1b710b35131c471bULL,
+  0x28db77f523047d84ULL, 0x32caab7b40c72493ULL, 0x3c9ebe0a15c9bebcULL, 0x431d67c49c100d4cULL,
+  0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL, 0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL
+};
+
+static int crypto_hashblocks(u8 *x,const u8 *m,u64 n) {
+  u64 z[8],b[8],a[8],w[16],t;
+  int i,j;
+
+  FOR(i,8) z[i] = a[i] = dl64(x + 8 * i);
+
+  while (n >= 128) {
+    FOR(i,16) w[i] = dl64(m + 8 * i);
+
+    FOR(i,80) {
+      FOR(j,8) b[j] = a[j];
+      t = a[7] + Sigma1(a[4]) + Ch(a[4],a[5],a[6]) + K[i] + w[i%16];
+      b[7] = t + Sigma0(a[0]) + Maj(a[0],a[1],a[2]);
+      b[3] += t;
+      FOR(j,8) a[(j+1)%8] = b[j];
+      if (i%16 == 15)
+	FOR(j,16)
+	  w[j] += w[(j+9)%16] + sigma0(w[(j+1)%16]) + sigma1(w[(j+14)%16]);
+    }
+
+    FOR(i,8) { a[i] += z[i]; z[i] = a[i]; }
+
+    m += 128;
+    n -= 128;
+  }
+
+  FOR(i,8) ts64(x+8*i,z[i]);
+
+  return n;
+}
+
+static const u8 iv[64] = {
+  0x6a,0x09,0xe6,0x67,0xf3,0xbc,0xc9,0x08,
+  0xbb,0x67,0xae,0x85,0x84,0xca,0xa7,0x3b,
+  0x3c,0x6e,0xf3,0x72,0xfe,0x94,0xf8,0x2b,
+  0xa5,0x4f,0xf5,0x3a,0x5f,0x1d,0x36,0xf1,
+  0x51,0x0e,0x52,0x7f,0xad,0xe6,0x82,0xd1,
+  0x9b,0x05,0x68,0x8c,0x2b,0x3e,0x6c,0x1f,
+  0x1f,0x83,0xd9,0xab,0xfb,0x41,0xbd,0x6b,
+  0x5b,0xe0,0xcd,0x19,0x13,0x7e,0x21,0x79
+} ;
+
+static int crypto_hash(u8 *out,const u8 *m,u64 n) {
+  u8 h[64],x[256];
+  u64 i,b = n;
+
+  FOR(i,64) h[i] = iv[i];
+
+  crypto_hashblocks(h,m,n);
+  m += n;
+  n &= 127;
+  m -= n;
+
+  FOR(i,256) x[i] = 0;
+  FOR(i,n) x[i] = m[i];
+  x[n] = 128;
+
+  n = 256-128*(n<112);
+  x[n-9] = b >> 61;
+  ts64(x+n-8,b<<3);
+  crypto_hashblocks(h,x,n);
+
+  FOR(i,64) out[i] = h[i];
+
+  return 0;
+}
+
+static void add(gf p[4],gf q[4]) {
+  gf a,b,c,d,t,e,f,g,h;
+  
+  Z(a, p[1], p[0]);
+  Z(t, q[1], q[0]);
+  M(a, a, t);
+  A(b, p[0], p[1]);
+  A(t, q[0], q[1]);
+  M(b, b, t);
+  M(c, p[3], q[3]);
+  M(c, c, D2);
+  M(d, p[2], q[2]);
+  A(d, d, d);
+  Z(e, b, a);
+  Z(f, d, c);
+  A(g, d, c);
+  A(h, b, a);
+
+  M(p[0], e, f);
+  M(p[1], h, g);
+  M(p[2], g, f);
+  M(p[3], e, h);
+}
+
+static void cswap(gf p[4],gf q[4],u8 b) {
+  int i;
+  FOR(i,4)
+    sel25519(p[i],q[i],b);
+}
+
+static void pack(u8 *r,gf p[4]) {
+  gf tx, ty, zi;
+  inv25519(zi, p[2]); 
+  M(tx, p[0], zi);
+  M(ty, p[1], zi);
+  pack25519(r, ty);
+  r[31] ^= par25519(tx) << 7;
+}
+
+static void scalarmult(gf p[4],gf q[4],const u8 *s) {
+  int i;
+  set25519(p[0],gf0);
+  set25519(p[1],gf1);
+  set25519(p[2],gf1);
+  set25519(p[3],gf0);
+  for (i = 255;i >= 0;--i) {
+    u8 b = (s[i/8]>>(i&7))&1;
+    cswap(p,q,b);
+    add(q,p);
+    add(p,p);
+    cswap(p,q,b);
+  }
+}
+
+static void scalarbase(gf p[4],const u8 *s) {
+  gf q[4];
+  set25519(q[0],X);
+  set25519(q[1],Y);
+  set25519(q[2],gf1);
+  M(q[3],X,Y);
+  scalarmult(p,q,s);
+}
+
+/* --- */
+
+static void keypair(unsigned char *pk, const unsigned char *sk) {
+  u8 h[64];
+  gf p[4];
+  crypto_hash(h, sk, 32);
+  h[0] &= 248;
+  h[31] &= 63;
+  h[31] |= 64;
+  scalarbase(p, h);
+  pack(pk, p);
+}
+
+__attribute__((noreturn)) static void fatal(const char *msg) {
+  (void)!write(2, "fatal: ", 7);
+  (void)!write(2, msg, strlen(msg));
+  (void)!write(2, "\n", 1);
+  exit(2);
+}
+
+static void randombytes(u8 *a, u32 size) {
+  int fd = open("/dev/urandom", O_RDONLY, 0);
+  int got;
+  if (fd < 0) fatal("open urandom");
+  got = read(fd, a, size);
+  if (got <= 0 || got + 0U != size) fatal("read urandom");
+  close(fd);
+}
+
+static char *append(char *p, char *pend, const char *input, u32 input_size) {
+  if (input_size > pend - p + 0U) fatal("append too long");
+  memcpy(p, input, input_size);
+  return p + input_size;
+}
+
+static const char b64chars[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+u32 base64encode_size(u32 size) {
+  return ((size + 2) / 3 * 4) + 1;  /* TODO(pts): Handle overflow. */
+}
+
+static char *base64encode(
+    char *p, char *pend, const u8 *input, u32 input_size) {
+  u32 i;
+  if (base64encode_size(input_size) > pend - p + 0U) fatal("base64 too long");
+  for (i = 0; i + 2 < input_size; i += 3) {  /* TODO(pts): Handle overflow. */
+    *p++ = b64chars[(input[i] >> 2) & 0x3F];
+    *p++ = b64chars[((input[i] & 0x3) << 4) |
+                    ((int) (input[i + 1] & 0xF0) >> 4)];
+    *p++ = b64chars[((input[i + 1] & 0xF) << 2) |
+                    ((int) (input[i + 2] & 0xC0) >> 6)];
+    *p++ = b64chars[input[i + 2] & 0x3F];
+  }
+  if (i < input_size) {
+    *p++ = b64chars[(input[i] >> 2) & 0x3F];
+    if (i == (input_size - 1)) {
+      *p++ = b64chars[((input[i] & 0x3) << 4)];
+      *p++ = '=';
+    } else {
+      *p++ = b64chars[((input[i] & 0x3) << 4) |
+                      ((int) (input[i + 1] & 0xF0) >> 4)];
+      *p++ = b64chars[((input[i + 1] & 0xF) << 2)];
+    }
+    *p++ = '=';
+  }
+  return p;
+}
+
+static void write_to_file(
+    const char *filename, unsigned mode, const char *p, u32 size) {
+  int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+  int got;
+  if (fd < 0) fatal("file open for write");
+  got = write(fd, p, size);
+  if (got < 0 || got + 0U != size) fatal("write file");
+  close(fd);
+}
+
+static char *build_openssh_public_key_ed25519(
+   char *p, char *pend, const u8 *public_key,
+   const char *comment, u32 comment_size) {
+  u8 ubuf[19 + 32];
+  static const char prefix[19] = "\0\0\0\x0bssh-ed25519\0\0\0 ";
+  static const char eprefix[12] = "ssh-ed25519 ";
+  static const char newline[1] = "\n";
+  memcpy(ubuf, prefix, 19);
+  memcpy(ubuf + 19, public_key, 32);
+  p = append(p, pend, eprefix, 12);
+  p = base64encode(p, pend, ubuf, 19 + 32);
+  p = append(p, pend, eprefix + 11, 1);  /* Space. */
+  p = append(p, pend, comment, comment_size);
+  p = append(p, pend, newline, 1);
+  return p;
+}
+
+static char *build_openssh_private_key_ed25519(
+   char *p, char *pend, const u8 *public_key,
+   const char *comment, u32 comment_size,
+   const u8 *private_key, const u8 *checkstr) {
+  static const char c_begin[36] = "-----BEGIN OPENSSH PRIVATE KEY-----\n";
+  static const char c_end[34] = "-----END OPENSSH PRIVATE KEY-----\n";
+  static const char c_kprefix[62] = "openssh-key-v1\0\0\0\0\4none\0\0\0\4none\0\0\0\0\0\0\0\1\0\0\0\x33\0\0\0\x0bssh-ed25519\0\0\0 ";
+  /* !! smaller size for data */
+  char data[4096], *origp, *dpend = data + sizeof data;
+  u32 data_size;
+  origp = p;
+  p = data;
+  p = append(p, dpend, c_kprefix, 62);
+  p = append(p, dpend, (const char*)public_key, 32);
+  data_size = p - data;  /* TODO(pts): Check for overflow. */
+
 #if 0
-void randombytes(u8 *a,u64 b) { (void)a; (void)b; }
+  ++ struct.pack(">L", 131 + len(comment) + (-(len(comment) + 3) & 7)),
+#endif
+  p = append(p, dpend, (const char*)checkstr, 4);
+  p = append(p, dpend, (const char*)checkstr, 4);
+#if 0
+      "\0\0\0\x0bssh-ed25519\0\0\0 ",
+#endif
+  p = append(p, dpend, (const char*)public_key, 32);
+#if 0
+      "\0\0\0@",
+#endif
+  p = append(p, dpend, (const char*)private_key, 32);
+  p = append(p, dpend, (const char*)public_key, 32);
+#if 0
+      struct.pack(">L", len(comment)),
+#endif
+  p = append(p, dpend, comment, comment_size);
+#if 0
+      "\1\2\3\4\5\6\7"[:-(len(comment) + 3) & 7]
 #endif
 
-int my_crypto_sign_ed25519_tweetnacl_keypair(unsigned char *pk, const unsigned char *sk) {
-    unsigned char h[64];
-    gf p[4];
-
-    /* randombytes(sk, 32); */
-    crypto_hash(h, sk, 32);
-    h[0] &= 248;
-    h[31] &= 63;
-    h[31] |= 64;
-
-    scalarbase(p, h);
-    pack(pk, p);
-
-    return 0;
-}
-
-static void print(const char *msg) {
-  (void)!write(1, msg, strlen(msg));
-}
-
-static char hextable[16] = "0123456789abcdef";
-
-static void printhex32(const u8 *data, char trailer) {
-  u8 buf[65], *p = buf;
-  unsigned i;
-  for (i = 0; i < 32; ++i) {
-    const unsigned char c = data[i];
-    *p++ = hextable[c >> 4];
-    *p++ = hextable[c & 15];
-  }
-  *p = trailer;
-  (void)!write(1, buf, 65);
+  /* !! apply line length 70 */
+  p = origp;
+  p = append(p, pend, c_begin, 36);
+  p = base64encode(p, pend, (const u8*)data, data_size);
+  p = append(p, pend, c_end, 34);
+#if 0
+  for i in xrange(0, len(data), 70):
+    output.append(data[i : i + 70])
+    output.append("\n")
+#endif
+  return p;
 }
 
 int main(int argc, char **argv) {
-  static const u8 private_key[32]        = "\x79\x20\x22\xc5\x58\xec\x6a\x70\x9c\x68\xac\x10\x90\xb6\xc1\x42\x5d\xa1\x9c\x66\xc7\x15\x64\x61\x8d\xd5\xad\x76\x36\x13\x51\x79";
-  static const u8 private_key_hashed[32] = "\x94\x1d\xae\xf5\xdc\x59\xd0\x92\x87\x50\x9c\x73\xa1\x48\x91\x56\x48\x31\x07\x69\xcf\x9f\xe5\x08\x28\x7d\x92\x49\x9d\xbd\x24\x0b";
-  static const u8 public_key_exp[32]     = "\x53\xb4\xc6\x18\xdb\x30\x01\xf1\x5a\x52\x9d\xd3\xdc\x86\x62\x87\x17\xb5\x60\x23\x10\xe7\xf3\x5c\x1f\xab\x0f\x69\xde\x61\xd9\x3f";
-  u8 public_key[32];
+  /* !! chmod */
+  u8 rnd36[36], public_key[32];
+  char buf[4096], *p, *pend = buf + sizeof buf;
+  static const char comment[1025] = "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello,  World!";
+  u32 comment_size;  /* TODO(pts): Check for overflow */
+  static const char filename[] = "id_t1";
+  static const char filename_pub[] = "id_t1.pub";  /* !! */
 
   (void)argc; (void)argv;
-  (void)private_key; (void)private_key_hashed;
-
-  my_crypto_sign_ed25519_tweetnacl_keypair(public_key, private_key);
-
-  print("private_key_hashed: "); printhex32(private_key_hashed, '\n');
-  print("public_key_exp:     "); printhex32(public_key_exp, '\n');
-  print("public_key:         "); printhex32(public_key, '\n');
+  comment_size = strlen(comment);
+  if (memchr(comment, '\n', comment_size) ||
+      memchr(comment, '\r', comment_size)) {
+    fatal("comment contains newline");
+  }
+  randombytes(rnd36, 36);
+  memcpy(rnd36, "xyztxyztxyztxyztxyztxyztxyztxyztCKst", 36);  /* !! */
+  keypair(public_key, rnd36);
+  p = buf;
+  p = build_openssh_public_key_ed25519(
+      p, pend, public_key, comment, comment_size);
+  write_to_file(filename_pub, 0644, buf, p - buf);
+  p = buf;
+  p = build_openssh_private_key_ed25519(
+      p, pend, public_key, comment, comment_size,
+      rnd36 /* private_key */, rnd36 + 4 /* checkstr */);
+  write_to_file(filename, 0600, buf, p - buf);
   return 0;
 }
